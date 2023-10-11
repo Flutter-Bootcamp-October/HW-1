23d9c2c047c821f832d7bf56f64a8b6af2e60efc
LMogdad <Zezoodj1200@gmail.com>
2023-10-11 19:09:56 +0300

    adding part 1: List

--- miqdad/bin/miqdad.dart
+++ miqdad/bin/miqdad.dart
@@ -1,3 +1,14 @@
-
+import 'package:miqdad/part1_list.dart' as list;
 void main(List<String> arguments) {
+  List listNumber = [3,6,10,9,4,30,57,1];
+  print("1. Print all the elements of a given list using a for loop.");
+  list.printAllElements(listNumber);
+  print("-----\n2. Calculate the sum of all the elements in a given list using a for loop.");
+  print(list.sumElements(listNumber));
+  print("-----\n3. Find the maximum element in a given list using a for loop.");
+  print(list.maxElement(listNumber));
+  print("-----\n4. Find the minimum element in a given list using a for loop.");
+  print(list.minElement(listNumber));
+  print("-----\n5. Print all the even elements of a given list using a for loop.");
+  list.printEven(listNumber);
 }
