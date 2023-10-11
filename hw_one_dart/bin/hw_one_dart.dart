58737d9d104227d18559600f529c07cfeaff11b8
Raffal Abu Eshey <86999942+Raff22@users.noreply.github.com>
2023-10-11 20:53:31 +0300

    added comments tried to make removePerson a return function but didn't know how to deal with returning null case

--- hw_one_dart/bin/hw_one_dart.dart
+++ hw_one_dart/bin/hw_one_dart.dart
@@ -1,5 +1,9 @@
-// import 'dart:html';
-
+/*
+    AUTHOR: Raffal Abu Eshey
+    FILE: hw_one_dart.dart
+    ASSIGNMENT: HW-1
+    PURPOSE: This file contains functions that deals with list and maps
+*/
 import 'package:hw_one_dart/hw_one_dart.dart' as hw_one_dart;
 
 // list methods:
@@ -11,9 +15,13 @@ void printAll(List list) {
   for (dynamic element in list) {
     print(element);
   }
-  // print("print done!");
 }
 
+/*
+  this function returns the sum of all elements in list.
+  parameter: List list
+  return: int listSum
+*/
 int sumALL(List list) {
   int listSum = 0;
   for (int element in list) {
@@ -22,6 +30,11 @@ int sumALL(List list) {
   return listSum;
 }
 
+/*
+  this function returns the maximum element in list
+  parameter: List list
+  return: int list Max
+*/
 int listMax(List list) {
   int listMax = list[0];
   for (int element in list) {
@@ -30,9 +43,13 @@ int listMax(List list) {
     }
   }
   return listMax;
-  // print("max done!");
 }
 
+/*
+  this function returns the minimum element in list
+  parameter: List list
+  return: int listMin
+*/
 int listMin(List list) {
   int listMin = list[0];
   for (int element in list) {
@@ -41,9 +58,12 @@ int listMin(List list) {
     }
   }
   return listMin;
-  // print("min done!");
 }
 
+/*
+  this function prints the even elements in list
+  parameter: List list
+*/
 void listEvens(List list) {
   for (int element in list) {
     if (element.isEven) {
@@ -53,18 +73,31 @@ void listEvens(List list) {
 }
 
 // map methods:
+/*
+  this function prints the keys of the map
+  parameter: Map map
+*/
 void printKeys(Map map) {
   for (var key in map.keys) {
     print(key);
   }
 }
 
+/*
+  this function prints the values of the map
+  parameter: Map map
+*/
 void printValues(Map map) {
   for (var value in map.values) {
     print(value);
   }
 }
 
+/*
+  this function returns the sum of values in map
+  parameter: Map map
+  return: int sum
+*/
 int sumValues(Map map) {
   int sum = 0;
   for (int value in map.values) {
@@ -73,6 +106,11 @@ int sumValues(Map map) {
   return sum;
 }
 
+/*
+  this function returns the key of the maximum value in map
+  parameter: Map map
+  return: String maxK
+*/
 String mapMax(Map map) {
   var maxV;
   var maxK;
@@ -91,6 +129,11 @@ String mapMax(Map map) {
   return maxK;
 }
 
+/*
+  this function returns the key of the minimum value in map
+  parameter: Map map
+  return: String minK
+*/
 String mapMin(Map map) {
   var minV;
   var minK;
@@ -110,18 +153,31 @@ String mapMin(Map map) {
 }
 
 // listOfMaps methods:
+/*
+  this function prints all the names in the maps of list
+  parameter: List list
+*/
 void printNames(List list) {
   for (Map dict in list) {
     print(dict["name"]);
   }
 }
 
+/*
+  this function prints all the ages in the maps of list
+  parameter: List list
+*/
 void printAges(List list) {
   for (Map dict in list) {
     print(dict["age"]);
   }
 }
 
+/*
+  this function returns the key of the oldest person in list of maps
+  parameter: List list
+  returns: String maxName
+*/
 String oldest(List list) {
   var maxName;
   var maxAge;
@@ -140,6 +196,11 @@ String oldest(List list) {
   return maxName;
 }
 
+/*
+  this function returns the key of the youngest person in list of maps
+  parameter: List list
+  returns: String minName
+*/
 String youngest(List list) {
   var minName;
   var minAge;
@@ -158,10 +219,18 @@ String youngest(List list) {
   return minName;
 }
 
+/*
+  this function adds a new map to list
+  parameter: List list, String n, int a
+*/
 void addPerson(List list, String n, int a) {
   list.add({"name": n, "age": a});
 }
 
+/*
+  this function remove a map to list
+  parameter: List list, String n, int a
+*/
 void removePerson(List list, String n) {
   for (Map dict in list) {
     if (dict["name"] == n) {
@@ -171,12 +240,18 @@ void removePerson(List list, String n) {
   }
 }
 
-//Sort the List of Maps created above by age in ascending order
+/*
+  this function sorts the List of Maps by age in ascending order
+  parameter: List list
+*/
 void sortAscending(List list) {
   list.sort((a, b) => a["age"].compareTo(b["age"]));
 }
 
-//Sort the List of Maps created above by name in descending order
+/*
+  this function sorts the List of Maps by name in descending order
+  parameter: List list
+*/
 void sortDescending(List list) {
   list.sort((b, a) => a["name"].compareTo(b["name"]));
 }
@@ -196,25 +271,24 @@ void main(List<String> arguments) {
   List listOfMaps = [
     {"name": "Alice", "age": 25},
     {"name": "Bob", "age": 30},
-    {"name": "Raffal", "age": 26},
-    {"name": "Charlie", "age": 35},
+    {"name": "Charlie", "age": 35}
   ];
-  // printAll(listNumber);
-  // print(sumALL(listNumber));
-  // print(listMax(listNumber));
-  // print(listMin(listNumber));
-  // listEvens(listNumber);
-  // printKeys(mapNumber);
-  // printValues(mapNumber);
-  // print(sumValues(mapNumber));
-  // print(mapMax(mapNumber));
-  // print(mapMin(mapNumber));
-  // printNames(listOfMaps);
-  // printAges(listOfMaps);
-  // print(oldest(listOfMaps));
-  // print(youngest(listOfMaps));
+  printAll(listNumber);
+  print(sumALL(listNumber));
+  print(listMax(listNumber));
+  print(listMin(listNumber));
+  listEvens(listNumber);
+  printKeys(mapNumber);
+  printValues(mapNumber);
+  print(sumValues(mapNumber));
+  print(mapMax(mapNumber));
+  print(mapMin(mapNumber));
+  printNames(listOfMaps);
+  printAges(listOfMaps);
+  print(oldest(listOfMaps));
+  print(youngest(listOfMaps));
   addPerson(listOfMaps, "David", 40);
-  // removePerson(listOfMaps, "Charlie");
+  removePerson(listOfMaps, "Charlie");
   sortAscending(listOfMaps);
   sortDescending(listOfMaps);
   print(listOfMaps);
