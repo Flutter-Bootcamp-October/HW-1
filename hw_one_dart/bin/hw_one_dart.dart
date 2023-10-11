9b8daf8c2f5fe99f813b652c4270a9adc8e2c098
Raffal Abu Eshey <86999942+Raff22@users.noreply.github.com>
2023-10-11 18:17:12 +0300

    created Rafal branch

---
+++ hw_one_dart/bin/hw_one_dart.dart
@@ -0,0 +1,5 @@
+import 'package:hw_one_dart/hw_one_dart.dart' as hw_one_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hw_one_dart.calculate()}!');
+}
---
+++ hw_one_dart/test/hw_one_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:hw_one_dart/hw_one_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
